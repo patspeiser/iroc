80566c830a567d28d13e0b866806d5cfd9f2a44a
pspeiser <pat@patspeiser.com>
2016-12-10 01:43:41 -0500

    api/rekogs returning rekogs in response object

--- server/routes/rekognition.js
+++ server/routes/rekognition.js
@@ -3,6 +3,7 @@ AWS.config.update({region: 'us-east-1'});
 const uuid = require('node-uuid');
 const rekognition = new AWS.Rekognition();
 const router = require('express').Router();
+var Promise = require('bluebird');
 module.exports = router;
 
 //specify version so it doesn't get fckd up and try to pick on its own
@@ -16,8 +17,8 @@ const s3 = new AWS.S3();
 //my bucket
 var bucketName = 'rekog-assets';
 
-//TODO: wrap this in a promise so I can .then() off of it
-var createRekogObject = function(asset, bucket, maxLabels){
+var createRekogObject = 
+function(asset, bucket, maxLabels){
 	var params = {
 		Image: {
 			S3Object: {
@@ -27,9 +28,14 @@ var createRekogObject = function(asset, bucket, maxLabels){
 		},
 		MaxLabels: maxLabels,
 	};
-	return rekognition.detectLabels(params, function(err, data){
-		if (err) console.log(err);
-		else console.log(data);
+	return new Promise(function(resolve, reject) {
+		rekognition.detectLabels(params, function(err, data){
+			if (err) {
+				console.log(err);
+			} else {
+				resolve(data);
+			}
+		});
 	});
 };
 
@@ -38,7 +44,9 @@ router.get('/', function(req, res, next){
 });
 
 router.post('/', function(req, res, next){
-	console.log(req.body);
-	createRekogObject(req.body.asset, req.body.bucket, req.body.maxLabels);
-	res.send('success');
+	createRekogObject(req.body.asset, req.body.bucket, req.body.maxLabels)
+	.then(function(rekogs){
+		res.send(rekogs);
+	})
+	.catch(next);
 });
