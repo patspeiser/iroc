aadcfb63547148584f596eb6f68ffdcc7c7bfc43
pspeiser <pat@patspeiser.com>
2016-12-10 12:24:44 -0500

    endpoint for uploading to s3 bucket

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,8 @@ module.exports = router;
 
 //api routes need to be listed here
 router.use('/rekog', require('./rekognition.js'));
-// router.use('/upload-to-s3', require('./upload-to-s3.js'));
+router.use('/s3', require('./s3.js'));
 
 router.get('/', function(req, res, next){
 	res.send('/api');
 });
-
-
