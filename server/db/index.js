cc73496457779051ba2b51153f2d01bc0c5af722
patspeiser <pat@patspeiser.com>
2016-12-16 14:26:53 -0500

    configged out the creds

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,10 @@
+const env = process.env.NODE_ENV || 'dev_aws';
+const config = require('../../config')[env];
 const Sequelize = require('Sequelize');
-const db = new Sequelize(process.env.DATABASE_URL, {});
+const db = new Sequelize(config.database.db, config.database.user, config.database.password, {
+	host: config.database.host,
+	dialect: 'postgres'
+});
 
 const Bucket = db.define('bucket', {
 	name: {
