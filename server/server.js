05056001572852999f885549fcd946a3f94b15fb
patspeiser <pat@patspeiser.com>
2017-07-13 12:26:10 -0400

    moved socket thing again

--- server/server.js
+++ server/server.js
@@ -10,41 +10,37 @@ const _ = require('lodash');
 
 var SYNC = process.env.SYNC || false; 
 
-
 if (SYNC){
-	console.log('SYNCING');
 	_db.sync({force: true})
-	.then(function(){
-		server.listen(port, function(){
-			//socket server event handling here
-			io.on('connection', function(socket){
-				console.log('SOCKET CONNECTION');
-				socket.on('s3UploadSuccess', function(payload){
-					console.log('got payload', payload);
-					rekog.createRekogObject(payload)
-					.then(function(rekogs){
-						console.log('got rekognition terms');
-						io.sockets.emit('rekogSuccess', rekogs);
-						console.log('emitting rekogs', rekogs);
-						rekogs.Labels.forEach(function(label){
-							var label = _.transform(label, function(result, val, key){
-								result[key.toLowerCase()] = val;
-							});
-							Label.create({
-								name: label.name,
-								confidence: label.confidence,
-								assetId: payload.id
-							});
-						});
-					});
-				});
+		.then(function(){
+			server.listen(port, function(){
+				console.log('listening on ' + port);
 			});
-		console.log('listening on ' + port);
+
+
 		});
-	});
 } else {
 	server.listen(port, function(){
 		console.log('listening on ' + port);
 	});
 }
 
+//socket server event handling here
+io.on('connection', function(socket){
+	socket.on('s3UploadSuccess', function(payload){
+		rekog.createRekogObject(payload)
+		.then(function(rekogs){
+			io.sockets.emit('rekogSuccess', rekogs);
+			rekogs.Labels.forEach(function(label){
+				var label = _.transform(label, function(result, val, key){
+					result[key.toLowerCase()] = val;
+				});
+			Label.create({
+				name: label.name,
+				confidence: label.confidence,
+				assetId: payload.id
+			});
+			});
+		});
+	});
+});
