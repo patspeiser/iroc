6d0c5a6e3b44d51c04fae8b13c1a5500b2e014b0
patspeiser <pat@patspeiser.com>
2017-07-13 12:36:23 -0400

    plz

--- server/server.js
+++ server/server.js
@@ -10,32 +10,33 @@ const _ = require('lodash');
 
 var SYNC = process.env.SYNC || false; 
 
+//socket server event handling here
+io.on('connection', function(socket){
+	socket.on('s3UploadSuccess', function(payload){
+		rekog.createRekogObject(payload)
+		.then(function(rekogs){
+			io.sockets.emit('rekogSuccess', rekogs);
+			rekogs.Labels.forEach(function(label){
+				var label = _.transform(label, function(result, val, key){
+					result[key.toLowerCase()] = val;
+				});
+			Label.create({
+				name: label.name,
+				confidence: label.confidence,
+				assetId: payload.id
+			});
+			});
+		});
+	});
+});
+
 if (SYNC){
 	_db.sync({force: true})
 		.then(function(){
 			server.listen(port, function(){
 				console.log('listening on ' + port);
 			});
-			//socket server event handling here
-			io.on('connection', function(socket){
-			socket.on('s3UploadSuccess', function(payload){
-				rekog.createRekogObject(payload)
-				.then(function(rekogs){
-					io.sockets.emit('rekogSuccess', rekogs);
-					rekogs.Labels.forEach(function(label){
-						var label = _.transform(label, function(result, val, key){
-							result[key.toLowerCase()] = val;
-						});
-					Label.create({
-						name: label.name,
-						confidence: label.confidence,
-						assetId: payload.id
-					});
-					});
-				});
-			});
 		});
-	});
 } else {
 	server.listen(port, function(){
 		console.log('listening on ' + port);
